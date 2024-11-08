e3ec71a6a8eaf8b58b69ad60a4fd08cd4ba45b05
Prelle <p.elayne.terry@gmail.com>
2024-11-07 22:46:05 -0700

    created project page with initial behavior and styling

---
+++ assets/js/project.js
@@ -0,0 +1,48 @@
+const emptyDiv = document.body.querySelector('#emptyDiv');
+const projectDiv = document.body.querySelector('#projectDiv');
+const nameElement = projectDiv.querySelector('#name');
+const descriptionElement = projectDiv.querySelector('#description');
+const taskListElement = projectDiv.querySelector('#taskList');
+const addTaskButton = projectDiv.querySelector('#addTask');
+
+function init() {
+    const project = readProject();
+
+    console.log(project);
+
+    if (project) {
+        nameElement.textContent = project.name;
+        descriptionElement.textContent = project.description;
+
+        if (project.tasks) {
+            // Replace the default text with a list of tasks that can be checked off
+            taskListElement.innerHTML = '';
+            const ul = document.createElement('ul');
+
+            for (let i = 0; i < project.tasks.length; i++) {            
+                const task = project.tasks[i];
+                
+                // Create an li for the task including the name and a Complete button
+                const li = document.createElement('li');
+                const p = document.createElement('p');
+                p.textContent = task.name;
+
+                const button = document.createElement('button');
+                button.dataset.taskId = i;
+                button.textContent = '✅';
+
+                li.appendChild(p);
+                li.appendChild(button);
+
+                ul.appendChild(li);
+            }
+
+            taskListElement.appendChild(ul);
+        }
+
+        emptyDiv.classList.add('d-none');
+        projectDiv.classList.remove('d-none');
+    }
+}
+
+init();
