79d2bd972d486c034300e7f88ca968210837e88a
Prelle <p.elayne.terry@gmail.com>
2024-11-12 11:38:07 -0700

    implement completing tasks

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -17,3 +17,18 @@ function readProject(){
     return JSON.parse(localStorage.getItem("project"));
 }
 
+function markTaskComplete(taskId) {
+    const project = readProject();
+
+    if (!project) {
+        return 'No project to update';
+    }
+
+    if (project.tasks.length <= taskId) {
+        return "Specified task doesn't exist";
+    }
+
+    project.tasks[taskId].isComplete = true;
+
+    localStorage.setItem('project', JSON.stringify(project));
+}
--- assets/js/project.js
+++ assets/js/project.js
@@ -22,19 +22,22 @@ function init() {
             for (let i = 0; i < project.tasks.length; i++) {            
                 const task = project.tasks[i];
                 
-                // Create an li for the task including the name and a Complete button
-                const li = document.createElement('li');
-                const p = document.createElement('p');
-                p.textContent = task.name;
+                if (!task.isComplete) {
+                    // Create an li for the task including the name and a Complete button
+                    const li = document.createElement('li');
+                    const p = document.createElement('p');
+                    p.textContent = task.name;
 
-                const button = document.createElement('button');
-                button.dataset.taskId = i;
-                button.textContent = '✅';
+                    const button = document.createElement('button');
+                    button.dataset.taskId = i;
+                    button.textContent = '✅';
+                    button.addEventListener('click', taskCompleteListener);
 
-                li.appendChild(p);
-                li.appendChild(button);
+                    li.appendChild(p);
+                    li.appendChild(button);
 
-                ul.appendChild(li);
+                    ul.appendChild(li);
+                }
             }
 
             taskListElement.appendChild(ul);
@@ -43,6 +46,16 @@ function init() {
         emptyDiv.classList.add('d-none');
         projectDiv.classList.remove('d-none');
     }
+}
+
+function taskCompleteListener(event) {
+    const button = event.target;
+    const taskId = button.dataset.taskId;
+    const project = readProject();
+
+    markTaskComplete(taskId);
+
+    window.location.reload();
 }
 
 init();
