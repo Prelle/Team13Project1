2e3ed7f53fff29ba6b657ed5b2e1a77d61f4e46b
Jerome4star <jeromefourstar@icloud.com>
2024-11-12 19:59:06 -0700

    added some html code and created page-specific js and css files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -17,3 +17,35 @@ function readProject(){
     return JSON.parse(localStorage.getItem("project"));
 }
 
+function markTaskComplete(taskId) {
+    const project = readProject();
+
+    if (!project) {
+        return 'No project to update';
+    }
+
+    if (project.tasks.length <= taskId) {
+        return "Specified task doesn't exist";
+    }
+
+    project.tasks[taskId].isComplete = true;
+
+    localStorage.setItem('project', JSON.stringify(project));
+}
+
+function addTask(name) {
+    const project = readProject();
+
+    if (!project) {
+        return 'No project to update';
+    }
+
+    const newTask = {
+        name,
+        isComplete: false
+    };
+
+    project.tasks.push(newTask);
+
+    localStorage.setItem('project', JSON.stringify(project));
+}
