611bf25c0e01ce9caf75c21d551f0751df00c38e
Jerome4star <jeromefourstar@icloud.com>
2024-11-12 22:31:24 -0700

    made it work better

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,7 @@
 function createProject(name, description) {
     if(localStorage.getItem('project')){
-        return 'Project already made';
+        console.log("Project Already Made");
+        return
     }
 
     const project = {
@@ -49,3 +50,4 @@ function addTask(name) {
 
     localStorage.setItem('project', JSON.stringify(project));
 }
+
